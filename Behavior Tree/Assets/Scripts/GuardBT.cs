a106b98373ce8d382da58df75fda6a1213bb0f00
catandladybug <98188215+catandladybug@users.noreply.github.com>
2024-03-28 18:34:52 -0700

    upload

---
+++ Behavior Tree/Assets/Scripts/GuardBT.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorTree;
+public class GuardBT : BehaviorTree.Tree
+{
+    public UnityEngine.Transform[] waypoints;
+
+    public static float speed = 2f;
+    public static float fovRange = 6f;
+    public static float attackRange = 1f;
+
+    protected override Node SetupTree()
+    {
+        Node root = new Selector(new List<Node>
+        {
+            //new Sequence(new List<Node>
+            //{
+            //    new CheckEnemyInAttackRange(transform),
+            //    new TaskAttack(transform),
+            //}),
+            new Sequence(new List<Node>
+            {
+                new CheckEnemyInFOVRange(transform),
+                new TaskGoToTarget(transform),
+            }),
+            new TaskPatrol(transform, waypoints),
+        });
+
+        return root;
+    }
+}
